d5adadbf79d5114ac1c0f0d668ab2c426eb946a4
RomanKadevich <romankadevich@gmail.com>
2023-08-10 00:42:55 +0300

    feat: add styles of input component

--- src/components/inputForm/inputForm.test.tsx
+++ src/components/inputForm/inputForm.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import InputForm from './inputForm';
 
-// test('input rendets', () => {
-
-// });
+test('input rendets', () => {
+  render(<InputForm name='name' id='0' placeholder='Ваш пароль' />);
+  const inputElement = screen.getByPlaceholderText(/Ваш пароль/i);
+  expect(inputElement).toBeInTheDocument();
+});
--- src/components/inputForm/inputForm.tsx
+++ src/components/inputForm/inputForm.tsx
@@ -1,16 +1,22 @@
+import './inputForm.scss';
+
 interface IInputForm {
-  name:string;
-  id:string;
-  className:string;
+  name: string;
+  id: string;
+  placeholder: string;
+  className?: string;
 }
 
-const InputForm =(props:IInputForm)=>{
-  return (<input
-    type="text"
-    id={props.id}
-    className={props.className}
-    name={props.name}
-  />);
+const InputForm = (props: IInputForm) => {
+  return (
+    <input
+      type='text'
+      id={props.id}
+      className={props.className ? `inputForm ${props.className}` : 'inputForm'}
+      name={props.name}
+      placeholder={props.placeholder}
+    />
+  );
 };
 
 export default InputForm;
