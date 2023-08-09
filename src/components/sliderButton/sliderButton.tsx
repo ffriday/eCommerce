21f79b40273f7f4905c3d6941cd959d90f701736
Dmitriy Bulgakov <4irtim@gmail.com>
2023-08-09 12:20:01 +0300

    feat: add slider button component

---
+++ src/components/sliderButton/sliderButton.tsx
@@ -0,0 +1,30 @@
+import { FC, useState } from 'react';
+import './sliderButton.sass';
+
+interface ISliderButton {
+  text: {first: string, second: string},
+  firstStep: boolean,
+  handler: () => void,
+  className?: string,
+}
+
+const SliderButton: FC<ISliderButton> = ({ text, firstStep, handler, className }) => {
+  const defaultClassName = 'slider__button';
+  const activeClassName = 'active';
+  const classNames = className ? `${defaultClassName} ${className}` : defaultClassName;
+  const [state, setState] = useState(firstStep);
+
+  const action = () => {
+    setState(!state);
+    handler();
+  };
+
+  return (
+    <div className={ classNames } onClick={action}>
+      <div className={ state ? activeClassName : '' }>{ text.first }</div>
+      <div className={ !state ? activeClassName : '' }>{ text.second }</div>
+    </div>
+  );
+};
+
+export default SliderButton;
