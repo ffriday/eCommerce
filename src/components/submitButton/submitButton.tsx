ff056f542d9e65246b8f93eb9b336dfe86e04513
Dmitriy Bulgakov <4irtim@gmail.com>
2023-08-09 10:00:29 +0300

    feat: add disabled style to submit button

--- src/components/submitButton/submitButton.tsx
+++ src/components/submitButton/submitButton.tsx
@@ -3,17 +3,19 @@ import './submitButton.sass';
 
 interface ISubmitButton {
   text: string,
+  disabled: boolean,
   className?: string,
 }
 
-export const SubmitButton: FC<ISubmitButton> = ({ text, className }) => {
+export const SubmitButton: FC<ISubmitButton> = ({ text, disabled, className }) => {
   const defaultClassName = 'submit__button';
   const classNames = className ? `${defaultClassName} ${className}` : defaultClassName;
 
   return (
     <button
       className={ classNames }
-      type='submit'>
+      type='submit'
+      disabled={ disabled }>
       { text }
     </button>
   );
