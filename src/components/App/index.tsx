4453fb031e0ac8914e0ce035f86c168dc1d4ed53
Dmitriy Bulgakov <4irtim@gmail.com>
2023-08-17 20:28:11 +0300

    feat: add styles for register components

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from '../Header';
 import LoginForm from '../loginForm/loginForm';
+import RegisterForm from '../registerForm/registerForm';
 import './styles.scss';
 
 export default function App() {
   return (
     <>
       <Header />
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <RegisterForm />
     </>
   );
 }
--- src/components/registerForm/registerForm.tsx
+++ src/components/registerForm/registerForm.tsx
@@ -1,27 +1,47 @@
-import { useState } from 'react';
+import { Context, createContext, useContext, useState } from 'react';
 import InputForm, { IInputAutocomplete, IInputForm } from '../inputForm/inputForm';
 import SliderButton from '../sliderButton/sliderButton';
 import './registerForm.scss';
 import Checkbox from '../checkbox/checkbox';
+import SubmitButton from '../submitButton/submitButton';
+
+interface IValidate {
+  email: boolean;
+  password: boolean;
+  name: boolean;
+  surename: boolean;
+  birthDate: boolean;
+  shipCountry: boolean;
+  billCountry: boolean;
+}
 
 const RegisterForm = () => {
+  const [validate, setValidate] = useState();
   const [firstPage, setFirstPage] = useState(true);
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+
+  const RegisterContext = createContext({ validate, setValidate });
 
   const sliderHandler = () => {
-    console.log('render');
     setFirstPage(!firstPage);
   };
 
   return (
-    <div className='register'>
-      <form className='register__form'>
-        <h1 className='register__headinf'>Регистрация</h1>
-        <p className='register__subtitle'>Создайте аккаунт, чтобы войти в личный кабинет</p>
-        <SliderButton text={{ first: 'Шаг 1', second: 'Шаг 2' }} handler={sliderHandler} />
-        {firstPage ? <RegisterStep1 /> : null}
-        {!firstPage ? <RegisterStep2 /> : null}
-      </form>
-    </div>
+    <RegisterContext.Provider value={{ validate, setValidate }}>
+      <div className='register'>
+        <form className='register__form'>
+          <h1 className='register__heading'>Регистрация</h1>
+          <p className='register__subtitle'>Создайте аккаунт, чтобы войти в личный кабинет</p>
+          <SliderButton text={{ first: 'Шаг 1', second: 'Шаг 2' }} handler={sliderHandler} firstStep={firstPage} className='register__slider' />
+          {firstPage ? <RegisterStep1 /> : null}
+          {!firstPage ? <RegisterStep2 /> : null}
+          <SubmitButton text='Зарегистрироваться' disabled={submitDisabled} className='register__submit' />
+          <span className='register__loginLink'>
+            У вас уже есть аккаунт? <a href='\login'>Войти</a>
+          </span>
+        </form>
+      </div>
+    </RegisterContext.Provider>
   );
 };
 
@@ -88,11 +108,11 @@ const RegisterStep1 = () => {
 
   return (
     <section className='register__step2'>
+      <InputForm {...firstNameFormProps} />
+      <InputForm {...lastNameFormProps} />
       <InputForm {...emailFormProps} />
       <InputForm {...passwordFormProps} />
       <InputForm {...passwordCheckFormProps} />
-      <InputForm {...firstNameFormProps} />
-      <InputForm {...lastNameFormProps} />
       <InputForm {...dateFormProps} />
     </section>
   );
@@ -138,26 +158,54 @@ const RegisterStep2 = () => {
     name: 'country',
     type: 'text',
     id: 'countrySelect',
-    placeholder: 'Страна',
+    placeholder: 'Выберите страну',
     inputClassName: 'register__input-country',
     labelClassName: 'register__label-country',
     propLabelInfo: 'Страна',
     autocomplete: countryAutocomplete,
   };
 
+  const buildingFormProps: IInputForm = {
+    name: 'building',
+    type: 'text',
+    id: 'building',
+    placeholder: 'Дом',
+    inputClassName: 'register__input-building',
+    labelClassName: 'register__label-building',
+    propLabelInfo: 'Дом',
+  };
+
+  const apartFormProps: IInputForm = {
+    name: 'apart',
+    type: 'text',
+    id: 'apart',
+    placeholder: 'Квартира',
+    inputClassName: 'register__input-apart',
+    labelClassName: 'register__label-apart',
+    propLabelInfo: 'Квартира',
+  };
+
   return (
     <section className='register__step2'>
       <div className=''>
-        <InputForm {...streetFormProps} />
-        <InputForm {...cityFormProps} />
-        <InputForm {...postalFormProps} />
         <InputForm {...countryFormProps} />
+        <InputForm {...cityFormProps} />
+        <InputForm {...streetFormProps} />
+        <div className='register__home'>
+          <InputForm {...buildingFormProps} />
+          <InputForm {...apartFormProps} />
+        </div>
+        <InputForm {...postalFormProps} />
       </div>
       <div>
-        <InputForm {...streetFormProps} />
-        <InputForm {...cityFormProps} />
-        <InputForm {...postalFormProps} />
         <InputForm {...countryFormProps} />
+        <InputForm {...cityFormProps} />
+        <InputForm {...streetFormProps} />
+        <div className='register__home'>
+          <InputForm {...buildingFormProps} />
+          <InputForm {...apartFormProps} />
+        </div>
+        <InputForm {...postalFormProps} />
       </div>
       <Checkbox id='checkbox' handler={() => 'test action'} classNameWrapper='register__checkbox' title='Same adress' />
     </section>
