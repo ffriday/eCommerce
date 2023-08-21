a2d7e1aa4b78eed35ed7e5e0fe29dce412c7a07f
RomanKadevich <romankadevich@gmail.com>
2023-08-21 16:48:50 +0300

    refactor: remove unnecessary imports

--- src/components/checkbox/checkbox.test.tsx
+++ src/components/checkbox/checkbox.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Checkbox from './checkbox';
 
 test('input rendets', () => {
