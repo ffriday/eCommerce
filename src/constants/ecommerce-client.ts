a6d848492e626e0f0906d7fc878b9408700445eb
Dmitriy Bulgakov <4irtim@gmail.com>
2023-08-24 22:17:59 +0300

    refactor: remove unused and commented code

--- src/constants/ecommerce-client.ts
+++ src/constants/ecommerce-client.ts
@@ -22,21 +22,6 @@ const authMiddlewareOptions: AuthMiddlewareOptions = {
   fetch: fetch,
 };
 
-// const passwordMiddlewareOptions: PasswordAuthMiddlewareOptions = {
-//   host: ENV.CTP_AUTH_URL,
-//   projectKey: projectKey,
-//   credentials: {
-//     clientId: ENV.CTP_CLIENT_ID,
-//     clientSecret: ENV.CTP_CLIENT_SECRET,
-//     user: {
-//       username: 'romankadevich@gmail.com',
-//       password: 'Password1',
-//     },
-//   },
-//   scopes,
-//   fetch: fetch,
-// };
-
 // Configure httpMiddlewareOptions
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
   host: ENV.CTP_API_URL,
@@ -50,19 +35,8 @@ export const ctpClient = new ClientBuilder()
   .withLoggerMiddleware() // Include middleware for logging
   .build();
 
-// Export the ClientBuilder
-// export const authClient = new ClientBuilder()
-//   .withClientCredentialsFlow(passwordMiddlewareOptions)
-//   .withHttpMiddleware(httpMiddlewareOptions)
-//   .withLoggerMiddleware() // Include middleware for logging
-//   .build();
-
 // Create apiRoot from the imported ClientBuilder and include your Project key
 export const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({ projectKey: ENV.CTP_PROJECT_KEY });
-
-// export const apiAuth = createApiBuilderFromCtpClient(authClient).withProjectKey({ projectKey: ENV.CTP_PROJECT_KEY });
-
-//TESTING
 
 export const customerLogin = async (email: string, password: string) => {
   const signIn: CustomerSignin = {
@@ -72,20 +46,6 @@ export const customerLogin = async (email: string, password: string) => {
 
   return apiRoot
     .login()
-    .post({
-      body: signIn,
-    })
-    .execute();
-};
-
-export const customerToken = async (email: string, password: string) => {
-  const signIn: CustomerSignin = {
-    email,
-    password,
-  };
-
-  return apiRoot
-    .customers()
     .post({
       body: signIn,
     })
