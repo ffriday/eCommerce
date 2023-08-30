bff74f2bc8a1013361ad88dbaa6a5b5c73852185
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2023-08-31 02:31:11 +0300

    feat: add catalog label

--- src/components/catalog/catalogNavigation.tsx
+++ src/components/catalog/catalogNavigation.tsx
@@ -13,14 +13,17 @@ interface ICatalogNavigation {
 
 const CatalogNavigation = ({ catalogData, startPage, page, limit, prevHandler, nextHandler }: ICatalogNavigation) => {
   const getTotalPageCount = (totalCount: number | undefined): number => Math.ceil(totalCount ? totalCount / limit : 1);
+  const countOfPages: number = getTotalPageCount(catalogData.totalCount);
+  const countOfLabel: number[] = new Array(countOfPages).fill(1);
+  const active = false;
 
   return (
     <nav className='catalog__navigation'>
       <button className='button catalog__button nav-button__left' onClick={prevHandler} disabled={page === startPage}></button>
-      <button
-        className='button catalog__button nav-button__right'
-        onClick={nextHandler}
-        disabled={page === getTotalPageCount(catalogData.totalCount)}></button>
+      {countOfLabel.map((item, index) => (
+        <div className={`catalog__label ${page === index + 1 ? 'catalog__label--active' : ''}`} key={index + item}></div>
+      ))}
+      <button className='button catalog__button nav-button__right' onClick={nextHandler} disabled={page === countOfPages}></button>
     </nav>
   );
 };
