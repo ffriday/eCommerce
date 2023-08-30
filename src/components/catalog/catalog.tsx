c07e30d7183b2b90ff202b60f2535639a0d01dda
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2023-08-31 00:47:39 +0300

    feat: add memo of product list and navigation

--- src/components/catalog/catalog.tsx
+++ src/components/catalog/catalog.tsx
@@ -1,18 +1,11 @@
 import { useState, useEffect, useMemo, useContext } from 'react';
 import './catalog.scss';
-import { ICardApiData } from '../../constants/types';
 import ProductAdapter from '../../constants/productAadapter';
 import { apiContext } from '../App';
-import ProductCard from '../card/card';
 import { ICatalogApiData } from '../../constants/types';
-
-// interface IProductCard {
-//   cardApiData?: ICardApiData;
-//   discounted?: boolean;
-// }
-
+import CatalogList from './catalogList';
 export default function ProductCatalog() {
-  const limit = 4;
+  const limit = 2;
   const startPage = 1;
   const [catalogData, setCatalogData] = useState<ICatalogApiData>({ products: [], totalCount: 0 });
   const [page, setPage] = useState<number>(startPage);
@@ -30,19 +23,12 @@ export default function ProductCatalog() {
   const prevButtonHandler = () => {
     const current: number = page;
     const prevPage: number = current - 1;
-    const totalCount = catalogData.products ? getTotalPageCount(catalogData.totalCount) : 0;
-
     setPage(prevPage > 0 ? prevPage : current), [page];
   };
   useEffect(() => {
     const getData = async () => {
       const offset: number = (page - 1) * limit;
       const catalogData: ICatalogApiData = await productAdapter.getCatalog({ limit: limit, offset: offset });
-      const totalCount: number | undefined = catalogData.totalCount;
-      // console.log(catalogData);
-      // for test using catalog KEY!!
-      // const catalogData= await productAdapter.getCatalog({limit:8, offset:0}) as ICardApiData[];
-      // console.log(catalogData);
       setCatalogData(catalogData);
     };
     getData();
@@ -51,14 +37,13 @@ export default function ProductCatalog() {
   return (
     <section className='catalog__section'>
       <div className='container'>
-        <div className='catalog__body'>
-          {catalogData.products.map((item) => {
-            return <ProductCard cardApiData={item} key={item.id} />;
-          })}
-        </div>
+        <CatalogList catalogData={catalogData} />
         <nav className='catalog__navigation'>
-          <button className='button catalog__button nav-button__left'></button>
-          <button className='button catalog__button nav-button__right'></button>
+          <button className='button catalog__button nav-button__left' onClick={prevButtonHandler} disabled={page === startPage}></button>
+          <button
+            className='button catalog__button nav-button__right'
+            onClick={nextButtonHandler}
+            disabled={page === getTotalPageCount(catalogData.totalCount)}></button>
         </nav>
       </div>
     </section>
