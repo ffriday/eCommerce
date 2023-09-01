cc2e9f12a9cff19bbf573153cfd64ea1eca2c4d4
mitrich <4irtim@gmail.com>
2023-09-01 12:33:13 +0300

    feat: add remove end edit status of address

--- src/constants/apiClient/apiClient.ts
+++ src/constants/apiClient/apiClient.ts
@@ -183,7 +183,7 @@ export default class ApiClient extends ApiBase {
     return customer.body.version;
   };
 
-  private getCustomerAddresses = async () => {
+  public getCustomerAddresses = async () => {
     const customer = await this.getCustomerInfo();
     return customer.body.addresses;
   };
@@ -237,6 +237,57 @@ export default class ApiClient extends ApiBase {
     const customerUpdate: MyCustomerUpdate = {
       version: version,
       actions: [action],
+    };
+
+    return api
+      .me()
+      .post({
+        body: customerUpdate,
+      })
+      .execute();
+  };
+
+  public changeAddressParams = async (
+    addressId: string,
+    shipment?: boolean,
+    billing?: boolean,
+    defaultShipment?: boolean,
+    defaultBilling?: boolean,
+  ) => {
+    const api = this.api.getAvalibleApi();
+    const version = await this.getCustomerVersion();
+    const actions: MyCustomerUpdateAction[] = [];
+
+    if (defaultShipment) actions.push({ action: 'setDefaultShippingAddress', addressId: addressId });
+    if (defaultBilling) actions.push({ action: 'setDefaultBillingAddress', addressId: addressId });
+    if (shipment) actions.push({ action: 'addShippingAddressId', addressId: addressId });
+    if (billing) actions.push({ action: 'addBillingAddressId', addressId: addressId });
+
+    const customerUpdate: MyCustomerUpdate = {
+      version: version,
+      actions: actions,
+    };
+
+    return api
+      .me()
+      .post({
+        body: customerUpdate,
+      })
+      .execute();
+  };
+
+  public removeAddress = async (addressId: string, removeFromShipment?: boolean, removeFromBilling?: boolean, remove?: boolean) => {
+    const api = this.api.getAvalibleApi();
+    const version = await this.getCustomerVersion();
+    const actions: MyCustomerUpdateAction[] = [];
+
+    if (remove) actions.push({ action: 'removeAddress', addressId: addressId });
+    if (removeFromShipment) actions.push({ action: 'removeShippingAddressId', addressId: addressId });
+    if (removeFromBilling) actions.push({ action: 'removeBillingAddressId', addressId: addressId });
+
+    const customerUpdate: MyCustomerUpdate = {
+      version: version,
+      actions: actions,
     };
 
     return api
