c695aa1d5a2ecc773f2c0a8b13c75a1abc7717b3
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2023-09-13 01:46:25 +0300

    feat: add and remove variants of products into basket

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useMemo, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductAdapter from '../../constants/productAadapter';
 import { apiContext } from '../App';
-
+import { HTTPResponseCode } from '../../constants/types';
 import ProductInfo from './productInfo';
 import Modal from 'react-modal';
 import { useMediaQuery } from '@react-hook/media-query';
@@ -47,37 +47,119 @@ export const Product = () => {
     setModalIsOpen(false);
   };
 
-  useEffect(() => {
-    const getData = async () => {
-      let product1: ICardApiData | undefined;
-      let product2: ICardApiData | undefined;
+  const [inBusketVar1, setInBusketVar1] = useState(false);
+  const [inBusketVar2, setInBusketVar2] = useState(false);
 
+  const getData = async () => {
+    let product1: ICardApiData | undefined;
+    let product2: ICardApiData | undefined;
+
+    try {
+      if (!key) {
+        return;
+      }
+
+      product1 = await productAdapter.getProductByKey({ key });
+      product2 = await productAdapter.getProductByKey({ key, productVariant: true });
+    } catch (error) {
+      product2 = undefined;
+    }
+
+    const products: ICardApiData[] = [];
+    if (product1?.image) products.push(product1);
+    if (product2?.image) products.push(product2);
+
+    if (isVariant) {
+      setProductData(product1);
+    } else {
+      setProductData(product2 || product1);
+    }
+
+    setProductsData(products);
+  };
+
+  const addItem = async (id: string, variantId: number) => {
+    try {
+      await api.addProductToCart(id, variantId);
+    } catch (err) {
+      throw new Error(`${err}`);
+    }
+  };
+  const addToBasketBtnHandler = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (productData) {
       try {
-        if (!key) {
-          return;
-        }
-
-        product1 = await productAdapter.getProductByKey({ key });
-        product2 = await productAdapter.getProductByKey({ key, productVariant: true });
-      } catch (error) {
-        product2 = undefined;
+        await addItem(productData.id, isVariant ? variantOfProduct1 : variantOfProduct2);
+        isInBusket();
+      } catch (err) {
+        throw new Error(`${err}`);
       }
+    }
+  };
 
-      const products: ICardApiData[] = [];
-      if (product1?.image) products.push(product1);
-      if (product2?.image) products.push(product2);
-
+  const getLineItemId = async () => {
+    try {
+      if (productData) {
+        const res = await api.getCart();
+        const lineId = res.body.lineItems.filter((item) => item.productId === productData.id)[0].id;
+        const variant = res.body.lineItems.filter((item) => item.productId === productData.id)[0].variant.id;
+        console.log(variant);
+        return { lineId, variant };
+      }
+    } catch (err) {
+      throw new Error(`${err}`);
+    }
+  };
+  const removeItem = async () => {
+    try {
+      const lineItemId = await getLineItemId();
+      if (lineItemId?.lineId) {
+        const variantIdToCheck = isVariant ? variantOfProduct1 : variantOfProduct2;
+        await api.removeProductFromCart(lineItemId.lineId, variantIdToCheck);
+      }
+    } catch (err) {
+      throw new Error(`${err}`);
+    }
+  };
+  const removeFromBasketBtnHandler = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (productData) {
+      try {
+        await removeItem();
+        isInBusket();
+      } catch (err) {
+        throw new Error(`${err}`);
+      }
+    }
+  };
+  const isInBusket = async () => {
+    const cart = await api.getCart();
+    console.log(cart);
+    if (cart.statusCode === HTTPResponseCode.ok) {
+      let variantIdToCheck: number;
       if (isVariant) {
-        setProductData(product1);
+        variantIdToCheck = 1;
       } else {
-        setProductData(product2 || product1);
+        variantIdToCheck = 2;
       }
 
-      setProductsData(products);
-    };
+      const itemInBasket = cart.body.lineItems.filter(
+        (lineItem) => lineItem.productId === productData?.id && lineItem.variant.id === variantIdToCheck,
+      );
+
+      const isInBasket = itemInBasket.length > 0;
+      if (isVariant) {
+        setInBusketVar1(isInBasket);
+      } else {
+        setInBusketVar2(isInBasket);
+      }
+    }
+  };
+  useEffect(() => {
     getData();
   }, [productAdapter, isVariant, key]);
 
+  useEffect(() => {
+    isInBusket();
+  }, [getData, isVariant]);
   return (
     <div className='product__container container'>
       <div className='slider__box'>
@@ -94,7 +176,9 @@ export const Product = () => {
           isActiveLabelClass={isVariant}
           cardApiData={productData}
           discounted={productData?.isDiscounted}
-          variant={isVariant ? variantOfProduct1 : variantOfProduct2}
+          inBusket={isVariant ? inBusketVar1 : inBusketVar2}
+          addToBasketBtnHandler={addToBasketBtnHandler}
+          removeFromBasketBtnHandler={removeFromBasketBtnHandler}
         />
       )}
       <Modal
--- src/components/product/productInfo.tsx
+++ src/components/product/productInfo.tsx
@@ -1,75 +1,21 @@
 import { apiContext } from '../App';
 import { useContext, useState, useEffect, useCallback } from 'react';
 import { ICardApiData } from '../../constants/types';
-import { HTTPResponseCode } from '../../constants/types';
+import { IMouthhandler } from '../../constants/types';
 import './product.scss';
 
 interface ProductInfo {
   isActiveLabelClass: boolean;
   cardApiData?: ICardApiData;
   discounted?: boolean;
-  variant?: number;
+  inBusket: boolean;
+  addToBasketBtnHandler: IMouthhandler;
+  removeFromBasketBtnHandler: IMouthhandler;
 }
 
-function ProductInfo({ discounted, cardApiData, isActiveLabelClass, variant = 2 }: ProductInfo) {
-  const [inBusket, setInBusket] = useState(false);
+function ProductInfo({ discounted, cardApiData, isActiveLabelClass, addToBasketBtnHandler, removeFromBasketBtnHandler, inBusket }: ProductInfo) {
   const data = cardApiData;
   const api = useContext(apiContext);
-  const addItem = async (id: string, variantId: number) => {
-    try {
-      await api.addProductToCart(id, variantId);
-      setInBusket(true);
-    } catch (err) {
-      throw new Error(`${err}`);
-    }
-  };
-  const addToBasketBtnHandler = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (data) {
-      await addItem(data.id, variant);
-
-      setInBusket(true);
-    }
-  };
-
-  const getLineItemId = async () => {
-    try {
-      if (data) {
-        const res = await api.getCart();
-        return res.body.lineItems.filter((item) => item.productId === data.id)[0].id;
-      }
-    } catch (err) {
-      throw new Error(`${err}`);
-    }
-  };
-  const removeItem = async () => {
-    try {
-      const lineItemId = await getLineItemId();
-      if (lineItemId) {
-        await api.removeProductFromCart(lineItemId, 1);
-      }
-    } catch (err) {
-      throw new Error(`${err}`);
-    }
-  };
-  const removeFromBasketBtnHandler = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (data) {
-      await removeItem();
-      setInBusket(false);
-    }
-  };
-  const isInBusket = async () => {
-    const cart = await api.getCart();
-    if (cart.statusCode === HTTPResponseCode.ok) {
-      const itemInBusket = cart.body.lineItems.filter((lineItem) => lineItem.productId === data?.id);
-      console.log(itemInBusket);
-      setInBusket(itemInBusket.length > 0);
-    }
-  };
-
-  useEffect(() => {
-    isInBusket();
-    console.log(inBusket);
-  }, [api]);
 
   const disableClassName = discounted ? 'card__price--disable' : '';
   const activeLabelClass = 'product__variants-label--active';
