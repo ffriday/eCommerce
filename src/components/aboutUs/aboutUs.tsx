818577d07ccbd1dc59c9234882b79e0d6bbc8304
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2023-09-17 14:12:27 +0300

    feat: add cooperation and rs

--- src/components/aboutUs/aboutUs.tsx
+++ src/components/aboutUs/aboutUs.tsx
@@ -7,6 +7,9 @@ import { difficultiesRoman } from './aboutUsData';
 import { infoDmitry } from './aboutUsData';
 import { contributionDmitry } from './aboutUsData';
 import { difficultiesDmitry } from './aboutUsData';
+import { cooperation } from './aboutUsData';
+import rsLogo from '../../assets/aboutus/rs-logo.svg';
+import rsImg from '../../assets/aboutus/rs-img.png';
 import './aboutUs.scss';
 
 const Aboutus = () => {
@@ -19,9 +22,18 @@ const Aboutus = () => {
       </section>
       <section className='about-us'>
         <h2 className='about-us__heading'>Сотрудничество</h2>
+        <div className='about-us__cooperation'>{cooperation}</div>
       </section>
       <section className='about-us'>
-        <h2 className='about-us__heading'>Чтобы узнать больше и начать обучение в RS School переходи по ссылке ниже</h2>
+        <h2 className='about-us__heading'>Чтобы узнать больше и начать обучение в RS School переходи по ссылкам ниже</h2>
+        <div className='about-us__rs'>
+          <a className='about-us__rs-logo-link' href=''>
+            <img className='about-us__rs-logo' src={rsLogo} alt='RS-school' />
+          </a>
+          <a className='about-us__rs-img-link' href=''>
+            <img className='about-us__rs-img' src={rsImg} alt='RS-school image' />
+          </a>
+        </div>
       </section>
     </main>
   );
