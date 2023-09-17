2db7cb278971ac3a6f26b02cfeb75c762d793927
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2023-09-17 15:05:14 +0300

    feat: add animation on scrool

--- src/components/aboutUs/aboutUs.tsx
+++ src/components/aboutUs/aboutUs.tsx
@@ -10,30 +10,47 @@ import { difficultiesDmitry } from './aboutUsData';
 import { cooperation } from './aboutUsData';
 import rsLogo from '../../assets/aboutus/rs-logo.svg';
 import rsImg from '../../assets/aboutus/rs-img.png';
+import FadeInOnScroll from './fadeScrool';
 import './aboutUs.scss';
 
 const Aboutus = () => {
   return (
     <main className='container'>
       <section className='about-us'>
-        <h2 className='about-us__heading'>Члены команды</h2>
-        <Bio mainInfo={mainDmitryInfo} info={infoDmitry} contribution={contributionDmitry} difficulties={difficultiesDmitry} />
-        <Bio mainInfo={mainRomanInfo} info={infoRoman} contribution={contributionRoman} difficulties={difficultiesRoman} />
+        <FadeInOnScroll fromLeft={true}>
+          <h2 className='about-us__heading'>Члены команды</h2>{' '}
+        </FadeInOnScroll>
+
+        <FadeInOnScroll>
+          <Bio mainInfo={mainDmitryInfo} info={infoDmitry} contribution={contributionDmitry} difficulties={difficultiesDmitry} />
+        </FadeInOnScroll>
+        <FadeInOnScroll fromLeft={true}>
+          <Bio mainInfo={mainRomanInfo} info={infoRoman} contribution={contributionRoman} difficulties={difficultiesRoman} />
+        </FadeInOnScroll>
       </section>
       <section className='about-us'>
-        <h2 className='about-us__heading'>Сотрудничество</h2>
-        <div className='about-us__cooperation'>{cooperation}</div>
+        <FadeInOnScroll>
+          <h2 className='about-us__heading'>Сотрудничество</h2>
+        </FadeInOnScroll>
+        <FadeInOnScroll fromLeft={true}>
+          {' '}
+          <div className='about-us__cooperation'>{cooperation}</div>
+        </FadeInOnScroll>
       </section>
       <section className='about-us'>
-        <h2 className='about-us__heading'>Чтобы узнать больше и начать обучение в RS School переходи по ссылкам ниже</h2>
-        <div className='about-us__rs'>
-          <a className='about-us__rs-logo-link' href=''>
-            <img className='about-us__rs-logo' src={rsLogo} alt='RS-school' />
-          </a>
-          <a className='about-us__rs-img-link' href=''>
-            <img className='about-us__rs-img' src={rsImg} alt='RS-school image' />
-          </a>
-        </div>
+        <FadeInOnScroll>
+          <h2 className='about-us__heading'>Чтобы узнать больше и начать обучение в RS School переходи по ссылкам ниже</h2>
+        </FadeInOnScroll>
+        <FadeInOnScroll fromLeft={true}>
+          <div className='about-us__rs'>
+            <a className='about-us__rs-logo-link' href=''>
+              <img className='about-us__rs-logo' src={rsLogo} alt='RS-school' />
+            </a>
+            <a className='about-us__rs-img-link' href=''>
+              <img className='about-us__rs-img' src={rsImg} alt='RS-school image' />
+            </a>
+          </div>
+        </FadeInOnScroll>
       </section>
     </main>
   );
